26f0dc401ab05ff3bcf14d16d780bd6521c4aafb
BENwada <domo0103wada@gmail.com>
2021-11-16 19:22:44 +0900

    20211116

--- src/data/index.js
+++ src/data/index.js
@@ -1,1 +1,32 @@
-export const
+export const MenuData = [
+  {
+    id: "1",
+    name: "肉まん",
+    price: "280",
+    recommendation: "true",
+    hard: "false",
+    image: "ImageOne",
+    description: "肉汁たっぷり、<br/>当店イチオシ！！",
+    alt: "肉まん",
+  },
+  {
+    id: "1",
+    name: "肉まん",
+    price: "280",
+    recommendation: "true",
+    hard: "false",
+    image: "ImageOne",
+    description: "肉汁たっぷり、<br/>当店イチオシ！！",
+    alt: "肉まん",
+  },
+  {
+    id: "1",
+    name: "肉まん",
+    price: "280",
+    recommendation: "true",
+    hard: "false",
+    image: "ImageOne",
+    description: "肉汁たっぷり、<br/>当店イチオシ！！",
+    alt: "肉まん",
+  },
+];
