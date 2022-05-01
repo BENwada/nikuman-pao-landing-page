55a461e01151095e2b98b58134c81a641fefb88f
BENwada <domo0103wada@gmail.com>
2022-05-02 08:44:33 +0900

    night-part-fixed

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -5,34 +5,41 @@ import styled from "styled-components";
 const InfoContainer = styled.div`
   width: 100vw;
   height: 50px;
-  background-color: #210842;
+  background-color: #330f61;
 `;
 
-const InformationLink = styled(Link)`
+const InformationLink = styled.div`
   width: 100vw;
+  height: 100%;
   color: white;
   cursor: pointer;
   text-decoration: none;
+  display: flex;
+  align-item: center;
+  justify-content: center;
 `;
 
-const Information = styled.div`
-  width: 100vw;
-  height: 100%;
-  padding-top: 12px;
-  display: flex;
-  justify-content: center;
-  align-item: center;
+const Information = styled.span`
+  margin-top: 10px;
+  font-size: 16px;
+
+  @media screen and (max-width: 480px) {
+    margin-top 13px;
+    font-size: 14px;
+  } ;
 `;
 
 const Info = () => {
   return (
     <>
       <InfoContainer>
-        <InformationLink to="/night-part">
-          <Information>
-            5月12日から夜の営業がスタートします。ページ増設中！
-          </Information>
-        </InformationLink>
+        <Link to="/night-part" style={{ textDecoration: "none" }}>
+          <InformationLink>
+            <Information>
+              5月12日から夜の営業がスタートします。ページ増設中！
+            </Information>
+          </InformationLink>
+        </Link>
       </InfoContainer>
     </>
   );
